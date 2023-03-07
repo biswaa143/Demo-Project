faed3f1ae1456b8a5a2eb8b3645f133bb8ca0a89
Biswa <barikbiswa143@gmail.com>
2023-03-07 22:28:27 +0530

    Demo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import AddUser from "./Users/AddUser";
 import UsersList from "./Users/UsersList";
 
@@ -15,10 +15,10 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
